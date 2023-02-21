44a3dd9967357d6bbc44f4cba3340437a977fba4
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-02-21 13:24:12 +0900

    added the pundit lines to tours_controller, added a booking_policy and al relevant lines to controller and policy. Focusing on the actions we want to implement for the first pitch

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,11 +1,21 @@
 class ToursController < ApplicationController
   def index
+    authorize @tour
   end
 
   def new
   end
 
   def create
+  end
 
+  def show
+    authorize @tour
+  end
+
+  private
+
+  def tour_params
+    params.require(:tour).permit(:name, :price, :description, :location)
   end
 end
